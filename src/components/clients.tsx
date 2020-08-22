9822d2636a97bc636a108d08a455312142eae7be
Daniel Mahon <danielmahon@users.noreply.github.com>
2020-08-22 13:30:22 -0400

    fix: fix client mobile render bug
    Signed-off-by: Daniel Mahon <danielmahon@users.noreply.github.com>

--- src/components/clients.tsx
+++ src/components/clients.tsx
@@ -5,6 +5,7 @@ import { graphql, useStaticQuery } from 'gatsby';
 import Img from 'gatsby-image';
 
 import 'swiper/swiper-bundle.min.css';
+import { useBreakpoints } from './hooks';
 
 SwiperCore.use([Autoplay]);
 
@@ -30,34 +31,37 @@ const query = graphql`
 
 export const Clients = () => {
   const data = useStaticQuery(query);
+  const { isXsmall } = useBreakpoints();
   const nodes = data?.images?.nodes;
 
   return (
-    <Swiper
-      spaceBetween={64}
-      slidesPerView={5}
-      loop={true}
-      style={{ height: 100 }}
-      autoplay={{
-        delay: 2000,
-        disableOnInteraction: false,
-      }}>
-      {nodes.map((node) => {
-        const image = node.childImageSharp;
+    <div style={{ padding: '1rem' }}>
+      <Swiper
+        spaceBetween={64}
+        slidesPerView={isXsmall ? 1 : 5}
+        loop={true}
+        style={{ height: 100 }}
+        autoplay={{
+          delay: 2000,
+          disableOnInteraction: false,
+        }}>
+        {nodes.map((node) => {
+          const image = node.childImageSharp;
 
-        if (image === null) return null;
+          if (image === null) return null;
 
-        return (
-          <SwiperSlide key={`${node.name}`}>
-            <Img
-              fluid={image.fluid}
-              fadeIn={false}
-              style={{ height: '100%' }}
-              imgStyle={{ objectFit: 'contain' }}
-            />
-          </SwiperSlide>
-        );
-      })}
-    </Swiper>
+          return (
+            <SwiperSlide key={`${node.name}`}>
+              <Img
+                fluid={image.fluid}
+                fadeIn={false}
+                style={{ height: '100%' }}
+                imgStyle={{ objectFit: 'contain' }}
+              />
+            </SwiperSlide>
+          );
+        })}
+      </Swiper>
+    </div>
   );
 };
