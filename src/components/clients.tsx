9168d27f673523db2fce21320aa27b2e2af8758e
Daniel Mahon <danielmahon@users.noreply.github.com>
2020-08-22 13:16:34 -0400

    feat: optimize images
    Signed-off-by: Daniel Mahon <danielmahon@users.noreply.github.com>

--- src/components/clients.tsx
+++ src/components/clients.tsx
@@ -5,7 +5,8 @@ import { Swiper, SwiperSlide } from 'swiper/react';
 // Import Swiper styles
 import 'swiper/swiper-bundle.min.css';
 import { graphql, useStaticQuery } from 'gatsby';
-import styled, { css } from 'styled-components';
+import styled from 'styled-components';
+import Img from 'gatsby-image';
 
 SwiperCore.use([Autoplay]);
 
@@ -15,40 +16,23 @@ const query = graphql`
       filter: { relativeDirectory: { eq: "clients" } }
       sort: { fields: name, order: ASC }
     ) {
-      edges {
-        node {
-          name
-          publicURL
-          relativeDirectory
+      nodes {
+        name
+        publicURL
+        relativeDirectory
+        childImageSharp {
+          fluid(maxWidth: 512) {
+            ...GatsbyImageSharpFluid
+          }
         }
       }
     }
   }
 `;
 
-const ClientWrapper = styled('div')`
-  height: 100%;
-  width: 100%;
-  display: flex;
-  /* align-content: center; */
-  justify-content: center;
-  /* overflow: hidden; */
-  /* position: relative; */
-`;
-const ClientImage = styled('img')`
-  /* width: 250px; */
-  /* display: block; */
-  max-width: 100%;
-  /* position: relative; */
-  /* display: inline-block; */
-  object-fit: contain;
-  /* max-height: 100%; */
-  flex: 0;
-`;
-
 export const Clients = () => {
   const data = useStaticQuery(query);
-  const edges = data?.images?.edges;
+  const nodes = data?.images?.nodes;
 
   return (
     <Swiper
@@ -59,17 +43,20 @@ export const Clients = () => {
       autoplay={{
         delay: 2000,
         disableOnInteraction: false,
-      }}
+      }}>
+      {nodes.map((node) => {
+        const image = node.childImageSharp;
 
-      // onSlideChange={() => console.log('slide change')}
-      // onSwiper={(swiper) => console.log(swiper)}>
-    >
-      {edges.map(({ node }) => {
+        if (image === null) return null;
+
         return (
           <SwiperSlide key={`${node.name}`}>
-            <ClientWrapper>
-              <ClientImage src={node.publicURL} alt={node.name} />
-            </ClientWrapper>
+            <Img
+              fluid={image.fluid}
+              fadeIn={false}
+              style={{ height: '100%' }}
+              imgStyle={{ objectFit: 'contain' }}
+            />
           </SwiperSlide>
         );
       })}
